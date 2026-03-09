38fd100d92a16ba26921c97c3bf7f9d22632290f
Ruslan Ibragimov <ruslan@heapy.io>
2026-03-09 08:25:49 +0300

    Rename project to "kotaml" and update package and copyright details.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 /*
 
    Copyright 2018-2023 Charles Korn.
+   Copyright 2026 Ruslan Ibrahimau.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -33,7 +34,7 @@ plugins {
     id("com.google.devtools.ksp") version "2.3.3"
 }
 
-group = "com.charleskorn.kaml"
+group = "io.heapy.kotaml"
 
 repositories {
     mavenCentral()
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,7 @@
 /*
 
    Copyright 2018-2023 Charles Korn.
+   Copyright 2026 Ruslan Ibrahimau.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -16,7 +17,7 @@
 
 */
 
-rootProject.name = "kaml"
+rootProject.name = "kotaml"
 
 pluginManagement {
     plugins {
